ae67e6167507c24473cb112f310e6da1ff7c1e4f
Synpse <tfaon@sapo.pt>
2019-12-05 16:48:14 +0000

    Bugfixing and changes to performance manager

--- LP2/Assets/PerformanceManager.cs
+++ LP2/Assets/PerformanceManager.cs
@@ -9,6 +9,11 @@ public class PerformanceManager : MonoBehaviour
 
     private void Update()
     {
+        CalculateDelta();
+    }
+
+    private void CalculateDelta()
+    {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
     }
 
@@ -17,23 +22,29 @@ public class PerformanceManager : MonoBehaviour
         int w = Screen.width, h = Screen.height;
 
         GUIStyle style = new GUIStyle();
-
-        Rect rect1 = new Rect(10, 10, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
         style.fontSize = h * 2 / 100;
         style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+
+        // Label1
+        Rect rect1 = new Rect(10, 10, w, h * 2 / 100);
+
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
         string text1 = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
 
         GUI.Label(rect1, text1, style);
 
+        // Label2
         Rect rect2 = new Rect(10, 30, w, h * 2 / 100);
+
         string text2 = $"Managed Heap: " +
             $"{Profiler.GetMonoHeapSizeLong() / 1024 / 1024} " +
             $"MB";
 
         GUI.Label(rect2, text2, style);
 
+        // Label3
+        // Label4
     }
 }
